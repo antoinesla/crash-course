18b0f64c54dd4155eeaf7e4bd162a7211d9ed51c
antoinesla <antoine.agricole@tuta.io>
2021-04-22 21:00:20 +0100

    added file not found error exercise

---
+++ files_and_exceptions/cat_and_dog_reader.py
@@ -0,0 +1,17 @@
+def display_file_content(filename):
+    '''displays content of file'''
+    try:
+        with open(filename) as f:
+            content = f.read()
+    except FileNotFoundError:
+        print('this file does not exist')
+    else:
+        print(content)
+
+filename = r'text_files\catss.txt'
+display_file_content(filename)
+
+filename = r'text_files\cats.txt'
+display_file_content(filename)
+filename = r'text_files\dogs.txt'
+display_file_content(filename)
